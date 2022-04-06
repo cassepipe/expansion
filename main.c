543fb616eff48db97c5f148d684ae195b2dd314c
cassepipe <cassepipe@ymail.com>
2022-04-06 12:04:39 +0200

    Fixed 11701cassepipe

--- main.c
+++ main.c
@@ -21,33 +21,33 @@ char **expand_simple_command(char *simple_command, t_strlist *env)
 	com = quote_preserving_split(simple_command);
 	if (!com)
 		return (NULL);
-	printf( "--Result of quote preserving split --\n");
-	print_strarray(com);
+	/*printf( "--Result of quote preserving split --\n");*/
+	/*print_strarray(com);*/
 
 	com = expand_args(com, env);
 	if (!com)
 		return (NULL);
-	printf( "--Now with variable expansion --\n" );
-	print_strarray(com);
+	/*printf( "--Now with variable expansion --\n" );*/
+	/*print_strarray(com);*/
 
 	expanded = ft_join_null_terminated_str_array(com);
 	free_strarray(com);
 	if (!expanded)
 		return (NULL);
-	printf( "--Expanded string--\n%s\n", expanded);
+	/*printf( "--Expanded string--\n%s\n", expanded);*/
 
 	com = split_on_unquoted_whitespace(expanded, " \t");
 	free(expanded);
 	if (!com)
 		return (NULL);
-	printf( "--Split on unquoted whitespace--\n" );
-	print_strarray(com);
+	/*printf( "--Split on unquoted whitespace--\n" );*/
+	/*print_strarray(com);*/
 
 	com = handle_quotes(com);
 	if (!com)
 		return (NULL);
-	printf("--After quote removal--\n");
-	print_strarray(com);
+	/*printf("--After quote removal--\n");*/
+	/*print_strarray(com);*/
 
 	return (com);
 }
@@ -62,8 +62,8 @@ t_strlist	*expand_pipeline(char *user_input, t_strlist *env)
 
 	arg_list = new_strlist();
 	pipeline = split_on_unquoted_redir(user_input, ">|<");
-	printf( "--Result of split on unquoted |, < and > --\n" );
-	print_strarray(pipeline);
+	/*printf( "--Result of split on unquoted |, < and > --\n" );*/
+	/*print_strarray(pipeline);*/
 	i = 0;
 	while (pipeline[i])
 	{
@@ -101,6 +101,7 @@ int main(int ac, char **av, char **envp)
 		free(user_input);
 		if (!list)
 			break;
+		print_strlist(*list);
 		free_strlist(list);
 	}
 	free_strlist(env);
